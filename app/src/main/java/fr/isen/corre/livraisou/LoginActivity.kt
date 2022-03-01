ae4ded8446ed03440289bb57055345679a86c1cd
PierreTinco <pierre.tinco@isen.yncrea.fr>
2022-03-01 14:47:06 +0100

    login/register activity

---
+++ app/src/main/java/fr/isen/corre/livraisou/LoginActivity.kt
@@ -0,0 +1,79 @@
+package fr.isen.corre.livraisou
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import fr.isen.corre.livraisou.MainActivity
+import fr.isen.corre.livraisou.RegisterActivity
+import fr.isen.corre.livraisou.databinding.ActivityLoginBinding
+
+
+
+class LoginActivity : AppCompatActivity() {
+    private  lateinit var binding: ActivityLoginBinding
+    private lateinit var auth: FirebaseAuth
+    val TAG = "RegisterActivity"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val database = Firebase.database
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        auth = Firebase.auth
+        listenClick()
+    }
+
+
+    private fun reload() {
+        TODO("Not yet implemented")
+    }
+
+    private fun listenClick() {
+        binding.LoginBtn.setOnClickListener {
+            auth.signInWithEmailAndPassword(
+                binding.EmailAddress.text.toString(),
+                binding.Password.text.toString()
+            )
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "signInWithEmail:success")
+                        val user = auth.currentUser
+                        changeActivityToMain()
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "signInWithEmail:failure", task.exception)
+                        Toast.makeText(
+                            baseContext, "Authentication failed.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+
+                    }
+                }
+        }
+
+        binding.button.setOnClickListener {
+            changeActivityToRegister()
+        }
+
+    }
+
+    private fun changeActivityToMain() {
+        startActivity(Intent(this, MainActivity::class.java))
+    }
+
+    private fun changeActivityToRegister() {
+        startActivity(Intent(this, RegisterActivity::class.java))
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+        TODO("Not yet implemented")
+    }
+
+}
---
+++ app/src/main/java/fr/isen/corre/livraisou/RegisterActivity.kt
@@ -0,0 +1,75 @@
+package fr.isen.corre.livraisou
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import fr.isen.corre.livraisou.databinding.ActivityRegisterBinding
+
+
+class RegisterActivity : AppCompatActivity() {
+    private  lateinit var binding: ActivityRegisterBinding
+    private lateinit var auth: FirebaseAuth
+    val TAG = "LoginActivity"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val database = Firebase.database
+        val TAG = "LoginActivity"
+        val myRef = database.getReference("post")
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        auth = Firebase.auth
+        listenClick()
+
+    }
+
+
+    private fun reload() {
+        TODO("Not yet implemented")
+    }
+
+    private fun listenClick() {
+        binding.RegisterBtn.setOnClickListener {
+            auth.createUserWithEmailAndPassword(binding.EmailAddress.text.toString().trim(),binding.Password.text.toString().trim())
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "createUserWithEmail:success")
+                        changeActivityToMain()
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                        Log.w(binding.EmailAddress.text.toString(), task.exception)
+                        Toast.makeText(baseContext, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
+
+        binding.button.setOnClickListener {
+            changeActivityToLogin()
+        }
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+        TODO("Not yet implemented")
+    }
+
+    private fun changeActivityToMain() {
+        startActivity(Intent(this, MainActivity::class.java))
+    }
+
+    private fun changeActivityToLogin() {
+        startActivity(Intent(this, LoginActivity::class.java))
+    }
+
+//test@gmail.com
+
+
+}
