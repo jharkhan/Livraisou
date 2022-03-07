2ecee2a80072e8392aa63e543ea4e51dabe64e12
PierreTinco <pierre.tinco@isen.yncrea.fr>
2022-03-07 11:19:26 +0100

    update bottom navigation bar

--- app/src/main/java/fr/isen/corre/livraisou/ProfileFragment.kt
+++ app/src/main/java/fr/isen/corre/livraisou/ProfileFragment.kt
@@ -10,7 +10,7 @@ import fr.isen.corre.livraisou.databinding.ActivityLoginBinding
 import fr.isen.corre.livraisou.databinding.FragmentProfileBinding
 import kotlinx.coroutines.Dispatchers.Main
 
-// TODO: Rename parameter arguments, choose names that match
+
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
@@ -22,44 +22,18 @@ private const val ARG_PARAM2 = "param2"
  */
 class ProfileFragment : Fragment() {
     private  lateinit var binding: FragmentProfileBinding
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-
-    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
-
+        binding = FragmentProfileBinding.inflate(layoutInflater)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ProfileFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic fun newInstance(param1: String, param2: String) =
-                ProfileFragment().apply {
-                    arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
-                    }
-                    listenClick()
-
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        listenClick()
     }
 
     private fun listenClick() {
@@ -71,18 +45,22 @@ class ProfileFragment : Fragment() {
             changeActivityToRegister()
         }
 
+        binding.btnMap.setOnClickListener {
+            changeActivityToMap()
+        }
     }
 
     private fun changeActivityToLogin() {
             val intent = Intent (activity, MainActivity::class.java)
-            activity?.startActivity(intent)
-
-
-
+            startActivity(intent)
     }
 
     private fun changeActivityToRegister() {
         val intent = Intent (activity, RegisterActivity::class.java)
+        startActivity(intent)
+    }
+    private fun changeActivityToMap() {
+        val intent = Intent (activity, MapsActivity::class.java)
         startActivity(intent)
     }
 }
