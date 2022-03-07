1ce9189fd4881c9fe15ed6e0dfbcbcdaeb022735
ahadhri9 <achrafhadhri9@gmail.com>
2022-03-07 16:23:56 +0100

    maps patch

--- app/src/main/java/fr/isen/corre/livraisou/MapsActivity.kt
+++ app/src/main/java/fr/isen/corre/livraisou/MapsActivity.kt
@@ -1,8 +1,12 @@
 package fr.isen.corre.livraisou
 
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.FragmentActivity
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -16,6 +20,10 @@ import fr.isen.corre.livraisou.databinding.ActivityMapsBinding
 class MapsActivity : FragmentActivity(), OnMapReadyCallback {
     private lateinit var binding: ActivityMapsBinding
     private var mMap: GoogleMap? = null
+    private var currentLocation: Location? = null
+    lateinit var locationManager: LocationManager
+
+
     var db: FirebaseFirestore? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -59,17 +67,31 @@ class MapsActivity : FragmentActivity(), OnMapReadyCallback {
                 )
 
                 // adding marker to each location on google maps
-                mMap!!.addMarker(MarkerOptions().position(location1).title("Marker"))
-
+                mMap!!.addMarker(MarkerOptions().position(location1).title("Casino"))
+                val zoomLevel = 16.0f //This goes up to 21
                 // below line is use to move camera.
-                mMap!!.moveCamera(CameraUpdateFactory.newLatLng(location1))
-                mMap!!.addMarker(MarkerOptions().position(location2).title("Marker"))
-                mMap!!.addMarker(MarkerOptions().position(location3).title("Marker"))
+                mMap!!.moveCamera(CameraUpdateFactory.newLatLngZoom(location1,zoomLevel))
+
+
+
+                mMap!!.addMarker(MarkerOptions().position(location2).title("Carrefour"))
+                mMap!!.addMarker(MarkerOptions().position(location3).title("Monoprix"))
             } else {
                 Toast.makeText(this@MapsActivity, "Error found is $error", Toast.LENGTH_SHORT)
                     .show()
             }
 
         }
+        // adding on click listener to marker of google maps.
+        mMap!!.setOnMarkerClickListener { marker -> // on marker click we are getting the title of our marker
+            // which is clicked and displaying it in a toast message.
+            val markerName = marker.title
+            Toast.makeText(this@MapsActivity, "Clicked location is $markerName", Toast.LENGTH_SHORT)
+                .show()
+            false
+        }
+
     }
+
+
 }
