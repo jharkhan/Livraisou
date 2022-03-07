d7f50c54a1107668ce1b93446cc803a190fece80
PierreTinco <pierre.tinco@isen.yncrea.fr>
2022-03-07 15:45:27 +0100

    update

--- app/src/main/java/fr/isen/corre/livraisou/ProfileFragment.kt
+++ app/src/main/java/fr/isen/corre/livraisou/ProfileFragment.kt
@@ -2,15 +2,23 @@ package fr.isen.corre.livraisou
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
+import com.google.firebase.ktx.Firebase
 import fr.isen.corre.livraisou.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
     private  lateinit var binding: FragmentProfileBinding
-
+    val TAG = "ProfileFragment"
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -21,6 +29,28 @@ class ProfileFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val database = Firebase.database
+        val userRef = database.getReference("user")
+        val user = Firebase.auth.currentUser
+        user?.let {
+            val uid = user.uid
+            // Read from the database
+            userRef.addValueEventListener(object: ValueEventListener {
+
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    // This method is called once with the initial value and again
+                    // whenever data at this location is updated.
+                    val value = snapshot.getValue<User>()
+
+
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    Log.w(TAG, "Failed to read value.", error.toException())
+                }
+
+            })
+        }
         listenClick()
     }
 
