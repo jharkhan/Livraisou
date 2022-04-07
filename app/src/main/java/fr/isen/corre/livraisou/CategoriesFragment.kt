e2a58adada6d66a8ec3769b1fb1bf7933645788c
Johanna Corre <johanna.corre@isen.yncrea.fr>
2022-04-07 10:16:06 +0200

    test

--- app/src/main/java/fr/isen/corre/livraisou/CategoriesFragment.kt
+++ app/src/main/java/fr/isen/corre/livraisou/CategoriesFragment.kt
@@ -41,7 +41,7 @@ class CategoriesFragment : Fragment() {
 
     }
 
-    val category = resources.getStringArray()
+    //val category = resources.getStringArray()
     companion object {
         /**
          * Use this factory method to create a new instance of
